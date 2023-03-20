2a804e83465e2aae69780abcf1dbec820a9b2fa8
monsursaleh <salehbog130@gmail.com>
2023-03-20 22:37:45 +0200

    adding portfolio and gallery

---
+++ README.md
@@ -0,0 +1,50 @@
+# Portfolio Project
+
+## Features
+
+- bg colour for the header on the scroll back to top button Hero-banner
+
+- Fixed header Animated hero-banner, background image fixed (not scrolling), the text is centred
+- horizontally and vertically. Projects
+
+- Three projects with styled cards (hover effects, filters, shadows) About me
+
+- Simple lore ipsum text with image Skills
+
+-sed icons, progress bar and pseudo-classes Footer
+
+- Contact form Gallery
+
+- Separate page with working menu Grid with images, random image sizes MOBILE
+
+- Same as a desktop but working nicely in 375px without horizontal scroll JS
+
+- Mobile menu with hamburger
+
+## Screenshots
+
+![Portfolio](/portfolio.png "Optional title")
+![Image Grid](/image_galary.png "Optional title")
+
+## Tech Stack
+
+**HTML**
+
+- In the skills section, add skills with icons and progress bars
+- Adding favicon
+- Validate code and fix issues
+  **CSS**
+- Header position fixed, onScroll with background
+- Hero-banner ::before or after color overlay
+- Use of CSS pseudo classes and elements - skills :nth-child() and icons
+- Styling of skills
+- Make sure the mobile version is still working fine
+- Validate CSS
+  **JS**
+- Add to the header background color on the scroll
+- Add back to top button
+- Add mobile menu
+
+## Authors
+
+- [@monsursaleh](https://www.github.com/monsursaleh)
---
+++ script.js
@@ -0,0 +1,54 @@
+const header = document.querySelector(".main-nav");
+const hamburger = document.querySelector("#mobileButton");
+const linksUl = document.querySelector("#links");
+const menulinks = document.querySelectorAll(".menulink");
+const menuItems = document.querySelectorAll("nav ul li a");
+const toTopButton = document.querySelector("#toTopButton");
+let scrollPos; // storing scroll postion
+let scrollPosAlt;
+
+// 1.fucntions work: if you scroll then from less then 200 then it will
+//back button will appear and header class bg classList willl add change header apperence
+// backButton display to none beforeHand
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+scrollFunction = () => {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    toTopButton.style.visibility = "visible";
+    header.classList.add("bg");
+  } else {
+    toTopButton.style.visibility = "hidden";
+    header.classList.remove("bg");
+  }
+};
+
+// 2 it will show  the items form hamburger manue in mobile - it
+const showMob = () => {
+  // getting the position when mobilemenu is opened
+  scrollPos = document.documentElement.scrollTop;
+  scrollPosAlt = document.body.scrollTop;
+  // toggling the menu visible/hidden;
+  linksUl.classList.toggle("hideMenu");
+};
+
+// lopingn every every menue link for any click  it will toggle ul links
+hamburger.addEventListener("click", showMob);
+
+for (const link of menulinks) {
+  link.addEventListener("click", showMob);
+}
+
+// 3.Function if you click this It will reach you top of the page
+
+const getToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; //
+};
+
+toTopButton.addEventListener("click", getToTop);
